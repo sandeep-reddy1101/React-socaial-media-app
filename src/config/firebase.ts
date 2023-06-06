4177b9219f474830bdb4ffe1e2cd82d1150f2877
sandeep-sandy-git <sandeep.x.sandy49@gmail.com>
2023-06-06 08:22:17 -0500

    ignoring config file

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -2,18 +2,12 @@
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
+import { firebaseConfig } from "./firebase-keys";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyDfGfLwpdJ1yYlGfq6HnU2kDfoP0wqaN9g",
-  authDomain: "react-oauth-df25e.firebaseapp.com",
-  projectId: "react-oauth-df25e",
-  storageBucket: "react-oauth-df25e.appspot.com",
-  messagingSenderId: "1067621916320",
-  appId: "1:1067621916320:web:519afaaa541b746bb173c5"
-};
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
